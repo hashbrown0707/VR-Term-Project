0034d5c166d5533c0ea621b540fbc6e029b2d1d5
sGxOxDs <62545840+sGxOxDs@users.noreply.github.com>
2021-09-21 19:52:23 +0800

    微更 門的創造
    RT
    我用絞鍊關節與動畫系統搞了一扇有開關動畫的門
    
    PS:
    碰撞箱是一切抖動元凶
    動畫控制要記得去建立參數

---
+++ VR Project/Assets/C#/Door.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Door : ObjectEvent
+{
+    public Animator dooranim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    new void Update()
+    {
+        base.Update();
+    }
+    //當 互動鍵被按下時
+    public override void Keydown()
+    {
+        if (state_i == 0)
+        {
+            dooranim.SetBool("開門", true);
+            state_i = 1;
+        }
+        else
+        {
+            dooranim.SetBool("開門", false);
+            state_i = 0;
+        }
+        Debug.Log("嘗試開關門");
+    }
+
+}
